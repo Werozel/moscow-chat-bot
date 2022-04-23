807ca54f759899955b4f15dd491650e196670186
Cactiw <Knyazev1514@gmail.com>
2022-04-23 14:25:52 +0300

    Fixed for old python-telegram-bot versions

--- globals.py
+++ globals.py
@@ -1,5 +1,5 @@
 import telegram.ext as tge
 from config import TELEGRAM_TOKEN
 
-updater = tge.Updater(TELEGRAM_TOKEN)
+updater = tge.Updater(TELEGRAM_TOKEN, use_context=True)
 dispatcher: tge.Dispatcher = updater.dispatcher
